8aaea231001c4cbcf98ee091398880d40fe45f2a
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-03-20 13:13:44 +0200

    Add main page

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,60 @@
-window.onload = function () {
+$( document ).ready(function() {
+
+	// --------------main navigation---------------------
+
+	$('.nav__link').on('click', function(e) {
+		let $subMenu = $(this).next('.nav__list-sub');
+		$('.nav__link, .nav__link-sub, .nav__list-sub').not($subMenu).filter('nav .active').removeClass('active');
+		if ($subMenu.length) {
+			e.preventDefault();
+			$(this).toggleClass('active');
+			$subMenu.toggleClass('active');
+		} 
+	});
+
+	$('.nav__link-sub').on('click', function(e) {
+		let $subMenu = $(this).next('.nav__list-sub');
+		$('.nav__link, .nav__link-sub, .nav__list-sub').not($subMenu).filter('nav .nav__list-sub.active .active').removeClass('active'); 
+		if ($subMenu.length) {
+			e.preventDefault();
+			$(this).toggleClass('active');
+			$subMenu.toggleClass('active');
+		} 
+	});
+
+	$('body').on('click', function(e) {
+		if( !$(e.target).is('.nav__link, .nav__link-sub') ) {
+			$('.nav__link, .nav__link-sub, .nav__list-sub').filter('nav .active').removeClass('active');
+			console.log('НЕ попал на ссылку');
+		}
+		console.log(e.target);
+	});
+
+	// --------------catalog accordion---------------------
+
+	$('.catalog__item').on('click', function(e) {
+		e.stopPropagation();
+		$(this).children('.catalog__item-text').toggleClass('active');
+		$(this).children('.catalog__list').slideToggle();
+	});
+
+	// --------------nav mobile---------------------
+
+	$('.header__btn-menu').on('click', function() {
+		$('.site-wrap, .nav-mobile-wrap, .nav-mobile-btn').addClass('active');
+
+	});
+
+	$('.nav-mobile-btn ').on('click', function() {
+		$('.site-wrap, .nav-mobile-wrap, .nav-mobile-btn').removeClass('active');
+	});
+
+	$('span.nav-mobile__text').on('click', function() {
+		$(this).toggleClass('active');
+		$(this).next('.nav-mobile__list').slideToggle();
+	});
+
 	
-	console.log('js is working!!!'); 
 
-}
 
-/* $( document ).ready(function() {
-
-	console.log( 'JQuery!!!' );
-
-}); */
+});
